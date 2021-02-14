c3f92291ff76c2e69bdf7de03bcf61322d278fd8
Emincan Ozcan <emincanozcann@gmail.com>
2021-02-14 19:29:39 +0300

    phpunit test suite added

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Emincan\Tracker\Tests;
+
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function testsAreRunning()
+    {
+        $this->assertTrue(true);
+    }
+}
