e74e8f8ef8ac1b55138a351f57c58fcd1e114649
Emincan Ozcan <emincanozcann@gmail.com>
2021-02-14 22:50:55 +0300

    eloquent created, updated and deleted events are tracked automatically

--- src/Trackable.php
+++ src/Trackable.php
@@ -6,7 +6,7 @@ use Emincan\Tracker\Models\TrackerActivity;
 
 trait Trackable
 {
-    public function saveActivity($action, $message = null)
+    public function saveActivity($action, $message = null, $additional_data = [])
     {
         $requestTracker = app()->make(RequestTracker::class);
         TrackerActivity::create([
@@ -15,7 +15,8 @@ trait Trackable
             "trackable_type" => get_class($this),
             "ip_address" => $requestTracker->getIpAddress(),
             "action" => $action,
-            "message" => $message
+            "message" => $message,
+            "additional_data" => $additional_data
         ]);
     }
 
@@ -25,5 +26,31 @@ trait Trackable
             "trackable_type" => get_class($this),
             "trackable_id" => $this->id
         ])->get();
+    }
+
+    public static function bootTrackable()
+    {
+        $events = ["created", "updated", "deleted"];
+        foreach ($events as $event) {
+            $message = class_basename(self::class) . " is " . $event;
+            self::$event(function ($model) use ($event, $message) {
+
+                $additionalData = [];
+                if ($event === "updated") $additionalData = ["original" => $model->getOriginal(), "changes" => $model->getChanges()];
+
+                $requestTracker = app()->make(RequestTracker::class);
+
+                TrackerActivity::create([
+                    "request_id" => $requestTracker->getRequestId(),
+                    "trackable_id" => $model->id,
+                    "trackable_type" => self::class,
+                    "ip_address" => $requestTracker->getIpAddress(),
+                    "action" => $event,
+                    "message" => $message,
+                    "additional_data" => $additionalData
+                ]);
+
+            });
+        }
     }
 }
